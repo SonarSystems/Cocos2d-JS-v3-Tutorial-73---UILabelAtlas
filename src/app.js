55d08fc9821a960303afae9452aed49835310f68
Frahaan Hussain <Sonar Systems>
2014-10-19 11:20:13 +0100

    UILabelAtlas

---
+++ src/app.js
@@ -0,0 +1,36 @@
+var HelloWorldLayer = cc.Layer.extend({
+    sprite:null,
+    ctor:function () {
+        //////////////////////////////
+        // 1. super init first
+        this._super();
+
+        /////////////////////////////
+        // 2. add a menu item with "X" image, which is clicked to quit the program
+        //    you may modify it.
+        // ask the window size
+        var size = cc.winSize;
+
+        var sprite = new cc.Sprite.create(res.CloseNormal_png);
+        sprite.setAnchorPoint(cc.p(0.5, 0.5));
+        sprite.setPosition(cc.p(size.width / 2, size.height / 2));
+        //this.addChild(sprite, 0);
+
+        var textAtlas = new ccui.TextAtlas();
+        textAtlas.setProperty("745", res.LabelAtlas_png, 17, 22, "0");
+        textAtlas.x = size.width / 2;
+        textAtlas.y = size.height / 2;
+        this.addChild(textAtlas);
+
+        return true;
+    }
+});
+
+var HelloWorldScene = cc.Scene.extend({
+    onEnter:function () {
+        this._super();
+        var layer = new HelloWorldLayer();
+        this.addChild(layer);
+    }
+});
+
